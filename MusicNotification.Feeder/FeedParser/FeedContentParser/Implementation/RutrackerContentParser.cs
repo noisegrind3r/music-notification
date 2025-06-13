e79924e40d27145a1c10ca0c2b1411d9e9e24a03
Shabashny.V <noisegrindermail@gmail.com>
2025-06-13 12:42:59 +0500

    fix: http client timeout

--- MusicNotification.Feeder/FeedParser/FeedContentParser/Implementation/RutrackerContentParser.cs
+++ MusicNotification.Feeder/FeedParser/FeedContentParser/Implementation/RutrackerContentParser.cs
@@ -20,7 +20,7 @@ public class RutrackerContentParser: IFeedContentParser
         using (var httpClient = new HttpClient(handler))
         {
             httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
-            httpClient.Timeout = TimeSpan.FromSeconds(30);
+            httpClient.Timeout = TimeSpan.FromSeconds(300);
 
             var responseBytes = await httpClient.GetByteArrayAsync(url);
 
